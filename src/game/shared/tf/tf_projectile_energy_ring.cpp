32e26d97ce0bd48340076a70df1ec7bcd23c8e6a
senmcgen <seanmcge@sas.upenn.edu>
2026-06-09 00:12:50 -0400

    fix: Compensate Bison Hitbox with increased tick speed

--- src/game/shared/tf/tf_projectile_energy_ring.cpp
+++ src/game/shared/tf/tf_projectile_energy_ring.cpp
@@ -64,7 +64,7 @@ void PrecacheRing(void *pUser)
 PRECACHE_REGISTER_FN(PrecacheRing);
 
 #ifdef GAME_DLL
-ConVar tf_bison_tick_time( "tf_bison_tick_time", "0.025", FCVAR_CHEAT );
+ConVar tf_bison_tick_time( "tf_bison_tick_time", "0.015", FCVAR_CHEAT );
 #endif
 
 
